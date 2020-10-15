1124867e31126d2ca2b49a3bca54f3385fdc5ad9
Ehsan Hamzei <hamzei.ehsan@gmail.com>
2020-10-15 17:26:05 +1100

    new method commit, tree, ner and events

--- ner.py
+++ ner.py
@@ -1,9 +1,11 @@
 import logging
-
+from tree import AnyNode, Tree
 from allennlp import pretrained
 
 logging.basicConfig(level=logging.INFO)
 model = pretrained.fine_grained_named_entity_recognition_with_elmo_peters_2018()
+parsemodel = pretrained.span_based_constituency_parsing_with_elmo_joshi_2018()
+
 up_name_tags = ['U-GPE', 'U-LOC', 'U-FAC', 'U-ORG']
 cp_name_tags = ['B-GPE', 'B-LOC', 'B-FAC', 'B-ORG', 'I-GPE', 'I-LOC', 'I-FAC', 'I-ORG', 'L-GPE', 'L-LOC', 'L-FAC',
                 'L-ORG']
@@ -11,6 +13,10 @@ cp_name_tags = ['B-GPE', 'B-LOC', 'B-FAC', 'B-ORG', 'I-GPE', 'I-LOC', 'I-FAC', '
 u_date_tags = ['U-DATE']
 cp_date_tags = ['B-DATE', 'I-DATE', 'L-DATE']
 
+u_event_tags = ['U-EVENT']
+cp_event_tags = ['B-EVENT', 'I-EVENT', 'L-EVENT']
+
+noun_phrase_tags = ['NN, NNS']
 
 class NER:
     @staticmethod
@@ -44,3 +50,24 @@ class NER:
     @staticmethod
     def extract_dates(sentence):
         return NER.extract_entities(sentence, u_date_tags, cp_date_tags)
+
+    @staticmethod
+    def extract_events(sentence):
+        return NER.extract_entities(sentence, u_event_tags, cp_event_tags)
+
+
+class CPARSER:
+    @staticmethod
+    def parse(sentence):
+        res = parsemodel.predict(sentence)
+        return res['hierplane_tree']['root']
+
+    @staticmethod
+    def construct_tree(sentence):
+        parse_results = CPARSER.parse(sentence)
+        return Tree(parse_results)
+
+
+# events = NER.extract_events('Which cities affected by the Hurricane Dorian?')
+# logging.info("www")
+tree = CPARSER.construct_tree('Which cities affected by the Hurricane Dorian by the sunset?')
---
+++ tree.py
@@ -0,0 +1,74 @@
+from anytree import AnyNode, RenderTree
+import anytree.cachedsearch as search
+
+
+# class NodeClass(NodeMixin):
+#     def __init__(self, name, nodeType, role='', parent=None, children=None):
+#         super(NodeClass, self).__init__()
+#         self.name = name
+#         self.role = role
+#         self.nodeType = nodeType
+#         if children:
+#             self.children = children
+#
+#     def get_name(self):
+#         return self.name
+#
+#     def get_role(self):
+#         return self.role
+#
+#     def set_role(self, role):
+#         self.role = role
+#
+#     def __repr__(self):
+#         return self.name+"{"+self.role+"} "
+
+
+class Tree:
+    def __init__(self, parse_dict):
+        self.parse_dict = parse_dict
+        self.tree = None
+        self.construct_tree()
+
+    def construct_tree(self):
+        root = AnyNode(name=self.parse_dict['word'], nodeType=self.parse_dict['nodeType'], role='')
+        temp = self.parse_dict
+
+        if 'children' in self.parse_dict.keys():
+            for child in self.parse_dict['children']:
+                self.add_to_tree(child, root)
+        self.root = root
+        self.tree = RenderTree(root)
+
+    def add_to_tree(self, node, parent):
+        n = AnyNode(name=node['word'], nodeType=node['nodeType'], parent=parent, role='')
+        if 'children' in node.keys():
+            for child in node['children']:
+                self.add_to_tree(child, n)
+
+    def render(self):
+        self.tree = RenderTree(self.root)
+
+    def __repr__(self):
+        if self.tree is None:
+            return "Empty Tree"
+        res = ""
+        for pre, fill, node in self.tree:
+            res+="%s%s (%s) {%s}" % (pre, node.name, node.nodeType, node.role)+"\n"
+        return res
+
+    def find_node_by_exact_name(self, string):
+         return search.findall_by_attr(self.root, string)
+
+    def find_node_by_name(self, string):
+        res = self.find_node_by_exact_name(string)
+        if len(res) > 0:
+            return res
+        return search.findall(self.root, filter_=lambda node: node.name in string.split())
+
+    def label_role(self, name, role):
+        nodes = self.find_node_by_name(name)
+        if len(nodes) == 1:
+            nodes[0].role = role
+            return True
+        return False
